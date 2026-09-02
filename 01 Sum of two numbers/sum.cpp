385458191a2cb62828d065356d946e261aba56ba
Manjit Baishya <manjitbaishya01@gmail.com>
2026-09-02 14:13:29 +0530

    added practice question 2

--- 01 Sum of two numbers/sum.cpp
+++ 01 Sum of two numbers/sum.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main(){
     int a=10, b=12;
+
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
+
     int sum = a+b;
     cout << "Sum is: " << sum;
     return 0;
---
+++ 02 Area of a Square/area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int s = 0;
+
+    cout << "Enter the side of the square: ";
+    cin >> s;
+
+    int area = s*s;
+    cout << "Area of the square is: " << area << endl;
+    return 0;
+}
